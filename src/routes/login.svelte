2a5791c6873715bd2853119a0d79f93c42132157
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-09 00:45:48 +0800

    chore: add navbar into login/register page

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,5 +1,14 @@
-<div class="bg-no-repeat bg-cover bg-center relative" style="background-image: url(https://images.unsplash.com/photo-1579621970563-ebec7560ff3e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80);"><div class="absolute bg-gradient-to-b from-green-500 to-green-400 opacity-75 inset-0 z-0"></div>
-  <div class="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
+<script>
+    import NavigationBar from "../components/NavigationBar.svelte"
+</script>
+
+
+<div class="min-h-screen">
+    <nav class="mb-10">
+        <NavigationBar></NavigationBar>
+    </nav>
+
+<div class="sm:flex sm:flex-row mx-0 justify-center">
       <div class="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
         <div class="self-start hidden lg:flex flex-col  text-white">
           <img src="" class="mb-3">
