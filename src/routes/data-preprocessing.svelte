e5c3efc6486bd5be9d248bf93309d976b65ff6ee
echo17666 <3239695085@qq.com>
2022-02-20 23:08:31 +0800

    feat: data describe

--- src/routes/data-preprocessing.svelte
+++ src/routes/data-preprocessing.svelte
@@ -1,1 +1,74 @@
+<script>
+    import { toast } from '@zerodevx/svelte-toast';
+    import { receiveBasicFileInfoApi } from '../api/fileApi'
+
+    import {
+		Button,
+        DataTable,
+		Pagination,
+	} from 'carbon-components-svelte';
+
+    let showTable = false;
+
+
+    let featureHeader = [
+        {key:'name',value:'name'},
+        {key:'count',value:'count'},
+        {key:'missing_rate',value:'missing_rate'},
+        {key:'mean',value:'mean'},
+        {key:'max',value:'max'},
+        {key:'min',value:'min'},
+        {key:'std',value:'std'},
+        {key:'median',value:'median'},
+        
+
+    ]
+
+
+    let basicDataContent = []
+    let filename = 'movie_data.xlsx'
+    function receiveBasicData() {
+		receiveBasicFileInfoApi(filename).then((response) => {
+			if (response.status == 200) {
+				console.log('response_data:', response.data)
+                basicDataContent = response.data.content
+                showTable = true;
+			} else {
+				console.log('error!');
+			}
+		});
+	}
+    let pagination = {
+		pageSize: 10,
+		page: 1
+	};
+
+
+
+</script>
+
+
+
+
 <h1>数据预处理</h1>
+<Button on:click={receiveBasicData} kind="tertiary">获取数据</Button>
+{#if showTable == true}
+<div class="container mx-auto">
+    <DataTable
+        size="compact"
+        sortable
+        title="原始表格"
+        description="数据:5"
+        headers={featureHeader}
+        pageSize={pagination.pageSize}
+        page={pagination.page}
+        rows={basicDataContent}
+    />
+    <Pagination
+        bind:pageSize={pagination.pageSize}
+        bind:page={pagination.page}
+        totalItems={basicDataContent.length}
+        pageSizeInputDisabled
+    />
+</div>
+{/if}
