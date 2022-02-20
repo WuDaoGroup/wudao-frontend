8c1e475f507d4b42f63bdfd8ac1bbc6683abedf2
LLLeoLi <1097261154@qq.com>
2022-02-20 15:47:54 +0800

    chores: datasetCharts

--- src/routes/data-observation.svelte
+++ src/routes/data-observation.svelte
@@ -1,7 +1,9 @@
 <script>
 	import LineChart from '../components/charts/LineChart.svelte';
+	import DChart from '../components/charts/DChart.svelte';
 </script>
 
 <h1>数据观察</h1>
 
 <LineChart title="Line" />
+<!-- <DChart title="DC"/> -->
