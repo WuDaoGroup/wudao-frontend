6df7d5264d9a40400c4e625629ca43f04aba288c
echo17666 <3239695085@qq.com>
2022-03-02 16:46:03 +0800

    fix: data flow in observation

--- src/components/ImagesChart.svelte
+++ src/components/ImagesChart.svelte
@@ -1,19 +1,18 @@
 <script>
 	let images = [];
 	import { Button } from 'carbon-components-svelte';
-	import { receiveBasicImageInfoApi } from '../api/fileApi';
+	import { receiveBasicFileInfoApi } from '../api/fileApi';
 	let showImage = false;
 	$: length = 0;
 
-	let filename = 'movie_data.xlsx';
 	function receiveData() {
-		receiveBasicImageInfoApi(filename).then((response) => {
+		receiveBasicFileInfoApi(localStorage.filename).then((response) => {
 			if (response.status == 200) {
 				console.log(response);
-				length = response.data.data;
+				let length = response.data.content.length;
 				for (let i = 1; i <= length; i++) {
 					let str =
-						`http://localhost:8123/static/images/${filename}_selected_features_` + i + '.png';
+						`http://wudao-backend.herokuapp.com/static/images/${localStorage.filename}_selected_features_` + i + '.png';
 					console.log(str);
 					let image = {
 						key: i,
