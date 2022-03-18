f1ededcc15106622f109218ba04922e96905c976
Yinghao Zhu <yhzhu99@gmail.com>
2022-03-19 00:30:56 +0800

    style: refactor data preprocessing stage style

--- src/routes/data-preprocessing.svelte
+++ src/routes/data-preprocessing.svelte
@@ -1,221 +1,119 @@
 <script>
-	import { toast } from '@zerodevx/svelte-toast';
-	import {baseLink} from '../services/api.js'
-
-	import {
-		receiveBasicFileInfoApi,
-		zscoreFilterInfoApi,
-		originZscoreApi,
-		confirmOriginZscoreApi
-	} from '../api/fileApi';
-	import {
-		Button,
-		DataTable,
-		Pagination,
-		Select,
-		SelectItem,
-		InlineNotification,
-		ProgressIndicator,
-		ProgressStep,
-		TextInput
-	} from 'carbon-components-svelte';
-	import { CodeSnippet } from 'carbon-components-svelte';
-	let showTable = false;
-	let hasSelected = false;
-	let hasFilled = false;
-	let basicData = {
-		content: [],
-		header: []
-	};
-
-	let currentIndex = 0;
-	let getFileData = { current: true, complete: false, invalid: false };
-	let chooseZscoreMethod = { current: false, complete: false, disabled: true, invalid: false };
-	let filterData = { current: false, complete: false, disabled: true, invalid: false };
-
-	const zscorechoice = ['请选择', '均值填充', '中位数填充'];
-	const defaultChoice = zscorechoice[0];
-	let zscoreType = { key: defaultChoice };
-	let code = [];
-	function receiveBasicData() {
-		receiveBasicFileInfoApi(localStorage.filename).then((response) => {
-			if (response.status == 200) {
-				console.log('response_data:', response.data);
-				code.push(response.data.code);
-				basicData.content = response.data.content;
-				basicData.header = response.data.header;
-				showTable = true;
-				getFileData.current = false;
-				getFileData.complete = true;
-				getFileData.invalid = false;
-				currentIndex++;
-				chooseZscoreMethod.current = true;
-				chooseZscoreMethod.disabled = false;
-				toast.push('归一化成功');
-			} else {
-				getFileData.invalid = true;
-				console.log('error!');
-				toast.push('归一化失败', {
-					theme: {
-						'--toastBackground': '#F56565',
-						'--toastBarBackground': '#C53030'
-					}
-				});
-			}
-		});
-	}
-	let bar;
-	function zscoreFilter() {
-		zscoreFilterInfoApi(localStorage.filename, bar).then((response) => {
-			if (response.status == 200) {
-				filterData.current = false;
-				filterData.complete = true;
-				currentIndex++;
-				toast.push('筛选成功');
-				console.log('LLLresponse_data:', response.data);
-				code.push(response.data.code);
-				console.log('code', code);
-				hasSelected = !hasSelected;
-			} else {
-				console.log('error!');
-			}
-		});
-	}
-
-	function confirmOriginZscore() {
-		console.log(zscoreType.key);
-		confirmOriginZscoreApi(localStorage.filename, zscoreType.key).then((response) => {
-			if (response.status == 200) {
-				console.log('response_data:', response.data);
-				code.push(response.data.code);
-				chooseZscoreMethod.invalid = false;
-				chooseZscoreMethod.current = false;
-				chooseZscoreMethod.complete = true;
-				currentIndex++;
-				filterData.disabled = false;
-				filterData.current = true;
-				toast.push('选择成功');
-				hasFilled = !hasFilled;
-			} else {
-				chooseZscoreMethod.invalid = true;
-				toast.push('请选择类型', {
-					theme: {
-						'--toastBackground': '#F56565',
-						'--toastBarBackground': '#C53030'
-					}
-				});
-				console.log('error!');
-			}
-		});
-	}
-
-	function downloadFile() {
-		const url = `${baseLink}/static/data/${localStorage.filename}_download_code.ipynb`;
-		const options = {
-			method: 'GET',
-			headers: {
-				Accept: 'application/octet-stream',
-				'Content-Type': 'application/octet-stream;charset=UTF-8'
-			}
-		};
-		fetch(url, options)
-			.then((response) => response.blob())
-			.then((data) => {
-				// console.log(data);
-				const url = window.URL.createObjectURL(new Blob([data]));
-				const link = document.createElement('a');
-				link.href = url;
-				link.setAttribute('download', 'code.ipynb'); //or any other extension
-				document.body.appendChild(link);
-				link.click();
-			});
-	}
-
-	let pagination = {
-		pageSize: 10,
-		page: 1
-	};
+    import { Tabs, Tab, TabContent } from "carbon-components-svelte";
 </script>
+
+
 
 <h1>数据预处理</h1>
 
-<ProgressIndicator bind:currentIndex spaceEqually preventChangeOnClick>
-	<ProgressStep
-		bind:current={getFileData.current}
-		bind:complete={getFileData.complete}
-		bind:invalid={getFileData.invalid}
-		label="Zscore Data"
-	/>
-	<ProgressStep
-		bind:current={chooseZscoreMethod.current}
-		bind:complete={chooseZscoreMethod.complete}
-		bind:invalid={chooseZscoreMethod.invalid}
-		bind:disabled={chooseZscoreMethod.disabled}
-		label="Fill Data"
-	/>
-	<ProgressStep
-		bind:current={filterData.current}
-		bind:complete={filterData.complete}
-		bind:invalid={filterData.invalid}
-		bind:disabled={filterData.disabled}
-		label="Filter Data"
-	/>
-</ProgressIndicator>
-<div class="grid grid-cols-5 gap-4">
-	<div class="col-span-2" />
-	<Button on:click={receiveBasicData} kind="tertiary">获取数据</Button>
-</div>
-{#if showTable == true}
-	<div class="container mx-auto">
-		<DataTable
-			size="compact"
-			sortable
-			title="原始表格"
-			description="数据:5"
-			headers={basicData.header}
-			pageSize={pagination.pageSize}
-			page={pagination.page}
-			rows={basicData.content}
-		/>
-		<Pagination
-			bind:pageSize={pagination.pageSize}
-			bind:page={pagination.page}
-			totalItems={basicData.content.length}
-			pageSizeInputDisabled
-		/>
-	</div>
-	<br />
-	<CodeSnippet class="m-auto" code={code[0]} type="multi" />
-{/if}
+<div class="divider"></div>
 
-<div class="grid grid-col-5">
-	<div class="col-span-1" />
-	<div class="col-span-3">
-		<Select inline labelText="选择填充方式" bind:selected={zscoreType.key}>
-			{#each zscorechoice as p}
-				<SelectItem text={p} value={p} />
-			{/each}
-		</Select>
-	</div>
-	<div class="col-span-1">
-		<Button on:click={confirmOriginZscore} kind="tertiary">确定</Button>
-	</div>
-</div>
-<br />
-{#if hasFilled == true}
-	<CodeSnippet class="m-auto" code={code[1]} type="multi" />
-{/if}
+<div class="flex w-full">
+    <div class="grid flex-grow card rounded-box place-items-center">
+        <Tabs>
+            <Tab label="Step 1: 数据标准化" />
+            <Tab label="Step 2: 缺失填充" />
+            <Tab label="Step 3: 离群值筛选" />
+            <Tab label="Step 4: 下载代码" />
+            <svelte:fragment slot="content">
+              <TabContent>
 
-<TextInput
-	inline
-	labelText="筛选数据"
-	type="Number"
-	placeholder="请填写筛选范围"
-	bind:value={bar}
-/>
-<Button on:click={zscoreFilter} kind="tertiary">数据筛选</Button>
-{#if hasSelected == true}
-	<CodeSnippet class="m-auto" code={code[2]} type="multi" />
-{/if}
+                <div class="hero" style="background-image: url(http://pic.616pic.com/bg_w1180/00/00/44/AWmPutVSXZ.jpg);">
+                    <div class="hero-overlay bg-opacity-60"></div>
+                    <div class="hero-content text-center text-neutral-content">
+                      <div class="max-w-md">
+                        <h2 class="mb-5 text-5xl font-bold">数据标准化</h2>
+                        <p class="mb-5">由于不同特征的数据的尺度、范围不同，为了消除特征的不齐性，我们需要将数据作标准化处理，使其scale到均值为0，方差为1的分布。</p>
+                        <button class="btn btn-primary">使用Z-Score标准化</button>
+                      </div>
+                    </div>
+                  </div>
 
-<Button kind="tertiary" on:click={downloadFile}>下载文件</Button>
+              </TabContent>
+              <TabContent>
+                <div class="hero" style="background-image: url(http://pic.616pic.com/bg_w1180/00/00/44/AWmPutVSXZ.jpg);">
+                    <div class="hero-overlay bg-opacity-60"></div>
+                    <div class="hero-content text-center text-neutral-content">
+                      <div class="max-w-md">
+                        <h2 class="mb-5 text-5xl font-bold">缺失值填充</h2>
+                        <p class="mb-5">对于机器学习模型而言，我们必须为每一个特征赋上值，因此需要作补全处理。常用的补全方式有：均值填充以及中位数填充。</p>
+                        <button class="btn btn-primary">开始填充缺失数据</button>
+                      </div>
+                    </div>
+                  </div>
+              </TabContent>
+              <TabContent>
+                <div class="hero" style="background-image: url(http://pic.616pic.com/bg_w1180/00/00/44/AWmPutVSXZ.jpg);">
+                    <div class="hero-overlay bg-opacity-60"></div>
+                    <div class="hero-content text-center text-neutral-content">
+                      <div class="max-w-md">
+                        <h2 class="mb-5 text-5xl font-bold">数据筛选</h2>
+                        <p class="mb-5">很多时候，数据中会出现异常分布，极大或极小，这通常是因为错误输入等原因导致。我们可根据Z-Score值的大小作筛选。</p>
+                        <button class="btn btn-primary">确定筛选方法</button>
+                      </div>
+                    </div>
+                  </div>
+              </TabContent>
+              <TabContent>
+
+                <div class="hero" style="background-image: url(http://pic.616pic.com/bg_w1180/00/00/44/AWmPutVSXZ.jpg);">
+                    <div class="hero-overlay bg-opacity-60"></div>
+                    <div class="hero-content text-center text-neutral-content">
+                      <div class="max-w-md">
+                        <h2 class="mb-5 text-5xl font-bold">代码下载</h2>
+                        <p class="mb-5">我们为您提供了当前位置所有数据处理部分的代码，以Python Jupyter Notebook的形式呈现，便于您的调试。</p>
+                        <button class="btn btn-primary">下载Jupyter代码</button>
+                      </div>
+                    </div>
+                  </div>
+
+              </TabContent>
+            </svelte:fragment>
+        </Tabs>
+    </div>
+    <div class="divider divider-horizontal"></div>
+    <div class="grid flex-grow card rounded-box place-items-center">
+        <h2>
+            经过处理后的数据统计信息
+        </h2>
+        
+        <div class="overflow-x-auto">
+            <table class="table table-zebra w-full">
+              <!-- head -->
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>统计量</th>
+                  <th>值</th>
+                </tr>
+              </thead>
+              <tbody>
+                <!-- row 1 -->
+                <tr>
+                  <th>1</th>
+                  <td>min</td>
+                  <td>m</td>
+                </tr>
+                <!-- row 2 -->
+                <tr>
+                  <th>2</th>
+                  <td>max</td>
+                  <td>Desktop Support Technician</td>
+
+                </tr>
+                <!-- row 3 -->
+                <tr>
+                  <th>3</th>
+                  <td>std</td>
+                  <td>Tax Accountant</td>
+                </tr>
+                <tr>
+                    <th>4</th>
+                    <td>std</td>
+                    <td>Tax Accountant</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+    </div>
+  </div>
