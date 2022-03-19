815d1718e3e987a293e7eab73ddaaba3829abf4f
Yinghao Zhu <yhzhu99@gmail.com>
2022-03-19 19:21:55 +0800

    chore: refactor data observation page layout

--- src/routes/data-observation.svelte
+++ src/routes/data-observation.svelte
@@ -1,6 +1,42 @@
 <script>
-	import ImagesChart from '../components/ImagesChart.svelte';
+	// import ImagesChart from '../components/ImagesChart.svelte';
+	import { onMount } from 'svelte';
+  	import {
+      Button,
+      DataTable,
+      Pagination,
+      Select,
+      SelectItem,
+      InlineNotification,
+      ProgressIndicator,
+      ProgressStep,
+      TextInput,
+      Tabs, Tab, TabContent
+    } from 'carbon-components-svelte';
 </script>
 
 <h1>数据观察</h1>
-<ImagesChart />
+
+<div class="divider"></div>
+
+<!-- <ImagesChart /> -->
+
+
+<div class="grid flex-grow card rounded-box place-items-center">
+	<Tabs>
+		<Tab label="原始数据观察" />
+		<Tab label="处理后数据观察" />
+		<svelte:fragment slot="content">
+
+		  <TabContent>
+
+		  </TabContent>
+
+		  <TabContent>
+
+
+
+		  </TabContent>
+		</svelte:fragment>
+	</Tabs>
+</div>
