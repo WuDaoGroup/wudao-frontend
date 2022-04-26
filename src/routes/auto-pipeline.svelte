8bc9d445cd9d0d0ae847afeb4d79a37bd665674b
Yinghao Zhu <yhzhu99@gmail.com>
2022-04-26 23:58:25 +0800

    style: add loading bar for autogluon page

--- src/routes/auto-pipeline.svelte
+++ src/routes/auto-pipeline.svelte
@@ -10,7 +10,8 @@
 		InlineNotification,
 		ProgressIndicator,
 		ProgressStep,
-        Tabs, Tab, TabContent
+    Tabs, Tab, TabContent,
+    Loading
 	} from 'carbon-components-svelte';
 	import { browser } from '$app/env';
 	import { toast } from '@zerodevx/svelte-toast';
@@ -178,6 +179,9 @@ instantUpload={false}
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-info flex-shrink-0 w-6 h-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span class="flex items-center ml-1">{currentState}</span>
                 </div>
+              </div>
+              <div class="flex flex-col items-center justify-center mt-8">
+                  <Loading withOverlay={false} />
               </div>
             {:else}
                 <div class="px-4 mx-auto container align-middle">
