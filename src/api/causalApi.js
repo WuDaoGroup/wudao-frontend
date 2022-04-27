71b46109a7edf02d1c917b72cd832215a9bd3d08
Yinghao Zhu <yhzhu99@gmail.com>
2022-04-27 23:12:10 +0800

    feat: notears causal api

--- src/api/causalApi.js
+++ src/api/causalApi.js
@@ -1,5 +1,24 @@
 import api from '../services/api.js';
 
+export const generateNotearsApi = async ( username, bar ) => {
+	try{
+		let form = new FormData()
+		form.append('username', username)
+		form.append('bar', bar)
+		const response = await api.post(`/api/v1/causal/notears`, form)
+		console.log(response)
+		return response
+	} catch (err){
+		console.error(err)
+	}
+}
+
+
+
+/////////////////////////////////////////////////
+// deprecated below
+///////////////////////////////////////////////
+
 export const causalModelBuildApi = async (data_filename, causalFeatures)=> {
     try{
         const response = await api.post(`/api/v1/causals/${data_filename}/model_build`, causalFeatures);
