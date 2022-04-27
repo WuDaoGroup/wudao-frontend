32c19a48477cc4108eec2366156fdb0299276b48
Yinghao Zhu <yhzhu99@gmail.com>
2022-04-27 23:09:55 +0800

    chore: init notears page

--- src/routes/causal-analysis/notears.svelte
+++ src/routes/causal-analysis/notears.svelte
@@ -16,7 +16,7 @@
     import {baseLink} from '../../services/api.js'
     import { toast } from '@zerodevx/svelte-toast';
     import { user } from '../../stores/userStore';
-    import {classificationTrainerApi} from '../../api/modelApi.js';
+    import { generateNotearsApi } from '../../api/causalApi.js';
     import { code } from '../../services/codeGen.js';
 
     let username;
@@ -25,14 +25,14 @@
       console.log('username:', username)
     });
 
-    let testPercent;
+    let bar;
 
-    function handleSplitDataset(){
-      if (testPercent >= 50 && testPercent <= 95){
-        toast.push('数据集划分成功')
+    function handleSplitBar(){
+      if (bar >= 0 && bar <= 100){
+        toast.push('阈值划分成功')
         return true
       } else {
-        toast.push('数据集划分失败，需在50%到95%之间', {
+        toast.push('阈值划分失败', {
             theme: {
               '--toastBackground': '#F56565',
               '--toastBarBackground': '#C53030'
@@ -43,31 +43,29 @@
     }
 
     // 当前状态
-    let currentState = '等待训练...';
+    let currentState = '等待生成...';
 
-    // 处理模型的返回结果
-    let modelResult=[]
-
-    function train(){
-      if (handleSplitDataset()==false) {
+    function generate(){
+      if (handleSplitBar()==false) {
         return
       }
-      currentState = '训练中...'
-      classificationTrainerApi(username, testPercent/100, 'adaboost').then((response) => {
+      currentState = '生成中...'
+      generateNotearsApi(username, bar/100, 'adaboost').then((response) => {
         if (response.status == 200) {
-          currentState = '完成训练'
+          currentState = '完成生成'
           console.log('response_data:', response.data);
           modelResult = response.data
-          toast.push('模型成功训练');
-          let image = document.getElementById('auroc-image');
-          image.src = `${baseLink}/static/data/${username}/images/adaboost/auroc.png`
+          toast.push('模型成功生成');
+          let image = document.getElementById('notears-image');
+          image.src = `${baseLink}/static/data/${username}/images/causal/notears.png`
           image.alt = `AUROC Curve`
         } else {
           console.log('error!');
-          toast.push('模型训练失败', {
+          toast.push('模型生成失败', {
             theme: {
               '--toastBackground': '#F56565',
               '--toastBarBackground': '#C53030'
+
             }
           });
         }
@@ -77,11 +75,11 @@
 
 
 
-<h1>Classification Model: AdaBoost</h1>
+<h1>因果发现</h1>
 
 <div class="card w-full">
   <div class="card-body">
-    <p>AdaBoost是一种迭代算法，其核心思想是针对同一个训练集训练不同的分类器(弱分类器)，然后把这些弱分类器集合起来，构成一个更强的最终分类器（强分类器）</p>
+    <p>使用 NO TEARS 方法绘制出因果关系的 DAG 图。参考论文：DAGs with NO TEARS: Continuous Optimization for Structure Learning</p>
   </div>
 </div>
 
@@ -90,8 +88,8 @@
 <div class="grid grid-cols-2 gap-4 w-full">
     <div class="grid flex-grow card rounded-box">
         <Tabs>
-            <Tab label="Step 1: 划分数据集" />
-            <Tab label="Step 2: 模型训练" />
+            <Tab label="Step 1: 划定阈值" />
+            <Tab label="Step 2: 生成因果图" />
 
             <svelte:fragment slot="content">
               <TabContent>
@@ -100,11 +98,11 @@
                     <div class="hero-overlay bg-opacity-60 rounded-lg"></div>
                     <div class="hero-content text-left text-neutral-content">
                       <div class="max-w-md">
-                        <h2 class="mb-5 text-5xl font-bold">数据集划分</h2>
-                        <p class="mb-5">在机器学习任务中，我们通常将已有的数据集划分为训练集和测试集两部分，其中训练集用来训练模型，而测试集则用来评估模型对于新样本的判别能力。</p>
+                        <h2 class="mb-5 text-5xl font-bold">划定阈值</h2>
+                        <p class="mb-5">不一定所有的特征变量之间都具有强因果关系，我们可以设置边系数阈值（0~1之间），而边系数阈值小于0阈值的点与边则会出现在因果图中。</p>
                         <div class="flex items-end justify-between px-4 pt-4 items-center">
-                          <input type="number" min="50" max="95" placeholder="输入训练集的比例(%)" class="input input-bordered input-primary text-zinc-900 w-[14rem]" bind:value={testPercent}>
-                          <button class="btn btn-primary" on:click={handleSplitDataset}>确定测试集划分比例</button>
+                          <input type="number" min="0" max="100" placeholder="输入阈值(%)" class="input input-bordered input-primary text-zinc-900 w-[14rem]" bind:value={bar}>
+                          <button class="btn btn-primary" on:click={handleSplitBar}>确定阈值大小</button>
                         </div>
                       </div>
                     </div>
@@ -117,9 +115,9 @@
                   <div class="hero-overlay bg-opacity-60 rounded-lg"></div>
                   <div class="hero-content text-left text-neutral-content">
                     <div class="max-w-md">
-                      <h2 class="mb-5 text-5xl font-bold">AdaBoost</h2>
-                      <p class="mb-5">炼丹师，开始你的训练！</p>
-                      <button class="btn btn-primary" on:click={train}>一键训练</button>
+                      <h2 class="mb-5 text-5xl font-bold">NO TEARS</h2>
+                      <p class="mb-5">一切准备就绪</p>
+                      <button class="btn btn-primary" on:click={generate}>点击生成因果关系图</button>
                     </div>
                   </div>
                 </div>
@@ -133,12 +131,11 @@
     <div class="grid flex-grow card rounded-box">
 
       <Tabs>
-          <Tab label="模型评估结果" />
-          <Tab label="AUROC 曲线" />
+          <Tab label="因果关系图" />
           <svelte:fragment slot="content">
             <TabContent>
 
-              {#if currentState != '完成训练'}
+              {#if currentState != '完成生成'}
                 <div class="alert shadow-lg mt-4">
                   <div>
                   <svg xmlns="http://www.w3.org/2000/svg" class="stroke-info flex-shrink-0 w-6 h-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
@@ -150,29 +147,13 @@
                 </div>
               {:else}
                   <div class="px-4 mx-auto container align-middle">
-                  <div class="grid grid-cols-4 gap-2">
-
-                      {#each modelResult as result}
-                      <div class="shadow rounded-lg py-4 px-5 bg-white">
-                          <div class="flex flex-row justify-center items-center">
-                          <div>
-                              <h6 class="text-sm">{result.indicator}</h6>
-                              <h4 class="text-black text-xl font-bold text-left">{result.value}</h4>
-                          </div>
-                          </div>
+                    <div class="px-4 mx-auto container align-middle h-[32rem]">
+                      <div class="flex flex-row justify-center items-center">
+                        <img src='../../favicon.png' alt='尚未加载' id='notears-image'/>
                       </div>
-                      {/each}
-
-                  </div>
+                    </div>
                   </div>
               {/if}
-            </TabContent>
-            <TabContent>
-              <div class="px-4 mx-auto container align-middle h-[32rem]">
-                <div class="flex flex-row justify-center items-center">
-                  <img src='../../favicon.png' alt='尚未加载' id='auroc-image'/>
-                </div>
-              </div>
             </TabContent>
           </svelte:fragment>
       </Tabs>
