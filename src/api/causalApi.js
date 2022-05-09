358c79732d58c8510263574bf18cceb632fbf6ac
Yinghao Zhu <yhzhu99@gmail.com>
2022-05-09 11:52:16 +0800

    chore: delete deprecated apis

--- src/api/causalApi.js
+++ src/api/causalApi.js
@@ -12,16 +12,3 @@ export const generateNotearsApi = async (username, bar) => {
 		console.error(err);
 	}
 };
-
-/////////////////////////////////////////////////
-// deprecated below
-///////////////////////////////////////////////
-
-export const causalModelBuildApi = async (data_filename, causalFeatures) => {
-	try {
-		const response = await api.post(`/api/v1/causals/${data_filename}/model_build`, causalFeatures);
-		return response;
-	} catch (err) {
-		console.error(err);
-	}
-};
