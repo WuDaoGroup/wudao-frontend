0c502c053c09a826badb894d5f5f220d8ee40f8e
Yinghao Zhu <yhzhu99@gmail.com>
2022-05-09 09:49:23 +0800

    refactor: clean unused files/pkgs

--- src/api/explanationApi.js
+++ src/api/explanationApi.js
@@ -50,34 +50,3 @@ export const generateCorrelationPairwiseApi = async (username)=> {
         console.error(err);
     }
 };
-
-/////////////////////////////////////////////////
-// deprecated below
-///////////////////////////////////////////////
-
-export const featureCorrApi = async (data_filename, methods)=> {
-    try{
-        const response = await api.post(`/api/v1/explanation/${data_filename}/feature_corr?methods=${methods}`);
-        return response;
-    }catch(err){
-        console.error(err);
-    }
-};
-
-export const objectMatrixApi = async (data_filename,featureCorrFeatures)=> {
-    try{
-        const response = await api.post(`/api/v1/explanation/${data_filename}/object_matrix`, featureCorrFeatures);
-        return response;
-    }catch(err){
-        console.error(err);
-    }
-};
-
-export const pairwiseFeatureCorrApi = async (data_filename, cols)=> {
-    try{
-        const response = await api.post(`/api/v1/explanation/${data_filename}/pairwise_feature_corr`, cols);
-        return response;
-    }catch(err){
-        console.error(err);
-    }
-};
